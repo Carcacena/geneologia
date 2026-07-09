24f2c9f7f3148bda5df0bb48d31b483957aad645
Carcacena <josegouveadepaula@gmail.com>
2026-07-09 19:32:44 -0300

    Update V4__insert__usuarios.sql

--- src/main/resources/db/migration/V4__insert__usuarios.sql
+++ src/main/resources/db/migration/V4__insert__usuarios.sql
@@ -1,2 +1,2 @@
 INSERT INTO `usuario` (`id`, `login`, `senha`, `perfil`, `tipo`) VALUES
-(1, 'jose', '$2a$10$E2UPv7arXmp3q0gHxNxXeOZuY49V9Sg7.nNl3A2n8Hn3Hn3Hn3Hn3', 'adm', 0);
+(1, 'jose', '123', 'adm', 0);
