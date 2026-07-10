0fdbd8d28329e9777a0f40c6151f7234649996cf
Carcacena <josegouveadepaula@gmail.com>
2026-07-09 22:57:10 -0300

    Update WebMvcConfig.java

--- src/main/java/br/com/jose/security/WebMvcConfig.java
+++ src/main/java/br/com/jose/security/WebMvcConfig.java
@@ -2,6 +2,7 @@
 package br.com.jose.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,8 +10,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Authorization", "Content-Type", "Accept", "X-Requested-With", "Cache-Control")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+
+    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        // Redireciona o acesso da raiz "/" para abrir o seu login.html automaticamente
         registry.addViewController("/").setViewName("forward:/login.html");
     }
 }
