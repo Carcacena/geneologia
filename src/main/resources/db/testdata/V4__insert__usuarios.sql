ea9c7b8decbb95f0d5cab6b3cc8075435b9821a7
jose gouvea paula <josegouveadepaula@gmail.com>
2026-08-21 06:37:08 -0300

    Insert multiple usuarios into the database

--- src/main/resources/db/testdata/V4__insert__usuarios.sql
+++ src/main/resources/db/testdata/V4__insert__usuarios.sql
@@ -1,2 +1,7 @@
 INSERT INTO `usuario` (`id`, `login`, `senha`, `perfil`, `tipo`) VALUES
-(1, 'jose', '$2a$10$R5eYlXDMNTmio8SElW/E2.zH53Tbnh/E2O5M2yMtey5cWBmXLmJPi', 'adm', 0);
+
+  INSERT INTO `usuario` (`id`, `login`, `senha`, `perfil`, `tipo`) VALUES
+(1, 'jose',
+ '$2a$10$R5eYlXDMNTmio8SElW/E2.zH53Tbnh/E2O5M2yMtey5cWBmXLmJPi', 'adm', 0);
+(2, 'conhecer', 
+'$2a$10$wIwjt4I9Hq3QY4wM7zJcj.iYJVjh4Z5NNpOuACvk6cYf2ExDNFqsy', 'adm', 0);
